b7b1dcde5ce7a4dacbdca8915b19554a492bf41e
Bhargav Parekh <42684274+nambvp03@users.noreply.github.com>
2018-09-09 00:08:51 -0700

    Update fail_rate.py
    commented not required code.
    change number of test to 3

--- fail_rate.py
+++ fail_rate.py
@@ -1,16 +1,16 @@
 import random
 
-number_of_test=30
+number_of_test=2
 mean=44829
 server=6
 iteration=100000
 hour=2
 sum_of_fail_rate=0
 avg_of_fail_rate=0
-standard_fail_rate=0.049
-error_percentage=0
-sum_of_error_percentage=0
-avg_of_error_percentage=0
+#standard_fail_rate=0.049
+#error_percentage=0
+#sum_of_error_percentage=0
+#avg_of_error_percentage=0
 
 def get_random_list():
 	server_list=[]
@@ -41,27 +41,26 @@ for test in range(0,number_of_test):
 		if server_fail_count>0:
 			fail_iteration +=1
 
-	#print("___",test+1,"___")
+	print("___",test+1,"___")
 	fail_rate=(fail_iteration*100)/iteration
-	#print("Fail Rate =",fail_rate,"%")
+	print("Fail Rate =",fail_rate,"%")
 
 	success_rate=100-fail_rate
-	#print("Success Rate =",success_rate,"%")
+	print("Success Rate =",success_rate,"%")
 
 	sum_of_fail_rate += fail_rate
 
-	sum_of_error_percentage += (fail_rate - standard_fail_rate)*100/standard_fail_rate
+	#sum_of_error_percentage += (fail_rate - standard_fail_rate)*100/standard_fail_rate
 
 
-
-print("__________________________")
+print("____________________________________")
 avg_of_fail_rate=sum_of_fail_rate/number_of_test
 print("Avg Fail Rate",avg_of_fail_rate,"%")
 
-print("__________________________")
-error_percentage=(avg_of_fail_rate - standard_fail_rate)*100/standard_fail_rate
-print("____Error percentage",error_percentage,"%")
+#print("_____________________________________")
+#error_percentage=(avg_of_fail_rate - standard_fail_rate)*100/standard_fail_rate
+#print("____Error percentage",error_percentage,"%")
 
-print("__________________________")
-avg_of_error_percentage=sum_of_error_percentage/number_of_test
-print("Avg Error percentage",avg_of_error_percentage,"%")
+#print("_____________________________________")
+#avg_of_error_percentage=sum_of_error_percentage/number_of_test
+#print("Avg Error percentage",avg_of_error_percentage,"%")
